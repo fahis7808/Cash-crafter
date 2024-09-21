2ef91eb987c7d2d5563aedeb6cc35f0a7f4f25db
fahis <afup7808@gmail.com>
2024-09-21 15:54:30 +0530

    home page

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,112 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:money_manage_app2/Pages/add_transaction_page.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:money_manage_app2/Model/db_model.dart';
-import 'package:money_manage_app2/Widgets/snackbar_info.dart';
-import 'package:money_manage_app2/Widgets/transaction_model.dart';
 import 'package:money_manage_app2/constant/app_colors.dart';
 import 'package:money_manage_app2/constant/app_font.dart';
 
-import '../Widgets/show_confirm_dialog.dart';
 import 'widget/custom_widget/custom_card.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late Box box;
-  DbHelper dbHelper = DbHelper();
-  Map? data;
-  int totalBalance = 0;
-  int totalIncome = 0;
-  int totalExpense = 0;
-  List<FlSpot> dataSet = [];
-  DateTime today = DateTime.now();
-
-  List<String> months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    box = Hive.box('cash');
-  }
-
-  Future<List<TransactionModel>> fetch() async {
-    if (box.values.isEmpty) {
-      return Future.value([]);
-    } else {
-      List<TransactionModel> items = [];
-      box.toMap().values.forEach((element) {
-        items.add(
-          TransactionModel(
-            element['amount'],
-            element['note'],
-            element['date'],
-            element['type'],
-          ),
-        );
-      });
-      return items;
-    }
-  }
-
-  List<FlSpot> getPlotPoints(List<TransactionModel> entireData) {
-    dataSet = [];
-    List tempdataSet = [];
-
-    for (TransactionModel item in entireData) {
-      if (item.date.month == today.month && item.type == "Expense") {
-        tempdataSet.add(item);
-      }
-    }
-    tempdataSet.sort((a, b) => a.date.day.compareTo(b.date.day));
-    for (var i = 0; i < tempdataSet.length; i++) {
-      dataSet.add(
-        FlSpot(
-          tempdataSet[i].date.day.toDouble(),
-          tempdataSet[i].amount.toDouble(),
-        ),
-      );
-    }
-    return dataSet;
-  }
-
-  getTotalBalance(List<TransactionModel> entireData) {
-    totalBalance = 0;
-    totalIncome = 0;
-    totalExpense = 0;
-    for (TransactionModel data in entireData) {
-      if (data.date.month == today.month) {
-        if (data.type == "Income") {
-          totalBalance += data.amount;
-          totalIncome += data.amount;
-        } else {
-          totalBalance -= data.amount;
-          totalExpense += data.amount;
-        }
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -118,10 +18,7 @@ class _HomePageState extends State<HomePage> {
               MaterialPageRoute(
                 builder: (context) => const TransactionPage(),
               ),
-            )
-                .then((value) {
-              setState(() {});
-            });
+            );
           },
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(
@@ -262,11 +159,14 @@ class _HomePageState extends State<HomePage> {
                                           color: AppColors.primaryColor,
                                           borderRadius: BorderRadius.all(
                                               Radius.circular(8))),
-                                      child: const Icon(
-                                        Icons.arrow_outward,
-                                        // Use the appropriate icon
-                                        color: Colors.white, // Icon color
-                                        size: 20, // Icon size
+                                      child: Transform.rotate(
+                                        angle: 3.5,
+                                        child: const Icon(
+                                          Icons.arrow_outward,
+                                          // Use the appropriate icon
+                                          color: Colors.white, // Icon color
+                                          size: 20, // Icon size
+                                        ),
                                       ),
                                     )
                                   ],
@@ -289,198 +189,5 @@ class _HomePageState extends State<HomePage> {
             ),
           ]),
         ));
-  }
-
-  Widget expenseTile(int value, String note, DateTime date, int index) {
-    return InkWell(
-      splashColor: Colors.teal,
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          deleteInfoSnackBar,
-        );
-      },
-      onLongPress: () async {
-        bool? answer = await showConfirmDialog(
-          context,
-          "WARNING",
-          "This will delete this record. This action is irreversible. Do you want to continue ?",
-        );
-        if (answer != null && answer) {
-          await dbHelper.deleteData(index);
-          setState(() {});
-        }
-      },
-      child: Container(
-        padding: const EdgeInsets.all(18.0),
-        margin: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          color: const Color(0xffced4eb),
-          borderRadius: BorderRadius.circular(
-            8.0,
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.arrow_circle_up_outlined,
-                          size: 28.0,
-                          color: Colors.red[700],
-                        ),
-                        const SizedBox(
-                          width: 4.0,
-                        ),
-                        const Text(
-                          "Expense",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    //
-                    Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: Text(
-                        "${date.day} ${months[date.month - 1]} ",
-                        style: TextStyle(
-                          color: Colors.grey[800],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "- $value",
-                      style: const TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    //
-                    Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: Text(
-                        note,
-                        style: TextStyle(
-                          color: Colors.grey[800],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget incomeTile(int value, String note, DateTime date, int index) {
-    return InkWell(
-      splashColor: Colors.teal,
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          deleteInfoSnackBar,
-        );
-      },
-      onLongPress: () async {
-        bool? answer = await showConfirmDialog(
-          context,
-          "WARNING",
-          "This will delete this record. This action is irreversible. Do you want to continue ?",
-        );
-
-        if (answer != null && answer) {
-          await dbHelper.deleteData(index);
-          setState(() {});
-        }
-      },
-      child: Container(
-        padding: const EdgeInsets.all(18.0),
-        margin: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          color: const Color(0xffced4eb),
-          borderRadius: BorderRadius.circular(
-            8.0,
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.arrow_circle_down_outlined,
-                      size: 28.0,
-                      color: Colors.green[700],
-                    ),
-                    const SizedBox(
-                      width: 4.0,
-                    ),
-                    const Text(
-                      "Credit",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
-                ),
-                //
-                Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: Text(
-                    "${date.day} ${months[date.month - 1]} ",
-                    style: TextStyle(
-                      color: Colors.grey[800],
-                    ),
-                  ),
-                ),
-                //
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "+ $value",
-                  style: const TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                //
-                //
-                Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: Text(
-                    note,
-                    style: TextStyle(
-                      color: Colors.grey[800],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
