9fb963b1b368ad421bbe66afefd7ce78d8511517
fahis <afup7808@gmail.com>
2024-10-08 13:44:43 +0530

    secure storage

---
+++ lib/service/secure_storage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LocalDB {
+  static const db = FlutterSecureStorage();
+
+  static saveToDB(String key, String value) async {
+    await db.write(key: key, value: value);
+  }
+
+  static readFromDB(String key) async {
+    String? data = await db.read(key: key);
+    return data;
+  }
+}
