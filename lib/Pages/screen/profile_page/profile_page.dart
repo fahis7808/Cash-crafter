39ab336f57c034f2c8cd057051af6b6c4738ec20
fahis <afup7808@gmail.com>
2024-10-09 23:11:34 +0530

    profile provider

--- lib/Pages/screen/profile_page/profile_page.dart
+++ lib/Pages/screen/profile_page/profile_page.dart
@@ -1,7 +1,5 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:money_manage_app2/Pages/screen/home_screen/home_page.dart';
 import 'package:money_manage_app2/Pages/widget/button/button.dart';
 import 'package:money_manage_app2/Pages/widget/text_field/custom_text_field.dart';
 import 'package:money_manage_app2/constant/app_colors.dart';
@@ -24,72 +22,75 @@ class ProfilePage extends StatelessWidget {
           builder: (context,data,_) {
             return Padding(
               padding: const EdgeInsets.fromLTRB(10, 30, 10, 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundColor: AppColors.containerColor,
-                    child: Text(
-                      "F",
-                      style: AppFont.text25,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundColor: AppColors.containerColor,
+                      child: Text(
+                        "F",
+                        style: AppFont.text25,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                   CustomTextField(
-                    value: data.userData.username,
-                    labelText: "Name",
-                   prefixIcon: FluentIcons.person_12_regular,
-                    // suffix: edit(),
-                     onChanged: (val){
-                      data.userData.username = val;
-                     },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                   CustomTextField(
-                    value: data.userData.phone,
-                    labelText: "Number",
-                    prefixIcon: FluentIcons.phone_16_regular,
-                    // suffix: edit(),
-                     keyboardType: TextInputType.number,
-                     onChanged: (val){
-                      data.userData.phone = val;
-                     },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                   CustomTextField(
-                    value: data.userData.email,
-                    labelText: "E-mail",
-                    prefixIcon: FluentIcons.mail_12_regular,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                   CustomTextField(
-                    value: data.userData.job,
-                    labelText: "Job",
-                    prefixIcon: FluentIcons.briefcase_32_regular,
-                    // suffix: edit(),
-                     onChanged: (val){
-                      data.userData.job = val;
-                     },
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  CustomButton(
-                      buttonText: "Update",
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => HomePage()));
-                      })
-                ],
+                    const SizedBox(
+                      height: 25,
+                    ),
+                     CustomTextField(
+                      value: data.userData.username,
+                      labelText: "Name",
+                     prefixIcon: FluentIcons.person_12_regular,
+                      // suffix: edit(),
+                       onChanged: (val){
+                        data.userData.username = val;
+                       },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                     CustomTextField(
+                      value: data.userData.phone,
+                      labelText: "Number",
+                      prefixIcon: FluentIcons.phone_16_regular,
+                      // suffix: edit(),
+                       keyboardType: TextInputType.number,
+                       onChanged: (val){
+                        data.userData.phone = val;
+                       },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                     CustomTextField(
+                      value: data.userData.email,
+                      labelText: "E-mail",
+                      prefixIcon: FluentIcons.mail_12_regular,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                     CustomTextField(
+                      value: data.userData.job,
+                      labelText: "Job",
+                      prefixIcon: FluentIcons.briefcase_32_regular,
+                      // suffix: edit(),
+                       onChanged: (val){
+                        data.userData.job = val;
+                       },
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    CustomButton(
+                        buttonText: "Update",
+                        onPressed: () {
+                          data.getData();
+                          // Navigator.push(context,
+                          //     MaterialPageRoute(builder: (context) => HomePage()));
+                        })
+                  ],
+                ),
               ),
             );
           }
--- lib/provider/profile_Provider.dart
+++ lib/provider/profile_Provider.dart
@@ -1,8 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:money_manage_app2/Model/user_model.dart';
+import 'package:money_manage_app2/service/secure_storage.dart';
 
-class ProfileProvider extends ChangeNotifier{
+class ProfileProvider extends ChangeNotifier {
   UserModel userData = UserModel();
 
+    String? uid;
+    ProfileProvider(){
+      getLoginID();
+    }
+    getLoginID()async{
+      uid = await LocalDB.readFromDB("LoginID");
+    }
+  getData() async {
+print(uid);
+    QuerySnapshot data = await FirebaseFirestore.instance
+        .collection("users")
+        .where("uid", isEqualTo: uid)
+        .get();
 
+    print(data.metadata);
+  }
 }
